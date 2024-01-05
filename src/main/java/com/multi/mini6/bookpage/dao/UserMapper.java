bba9314f6e361d6a900c82275e95c64b0235116a
myoungsuk <k4858678@gmail.com>
2024-01-05 10:23:56 +0900

    코드 통합 1차 회원 로그인 작업 및 설정 작업

--- src/main/java/com/multi/mini6/bookpage/dao/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.multi.mini6.bookpage.dao;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserMapper {
-}
--- src/main/java/com/multi/mini6/loginpage/vo/memberVO.java
+++
@@ -1,4 +0,0 @@
-package com.multi.mini6.loginpage.vo;
-
-public class memberVO {
-}
